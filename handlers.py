b1983a32608e0c2a4de12764ff0e81ee8209d198
vit <vit@yandex.ru>
2025-01-22 21:12:49 +0300

    finished user form

--- handlers.py
+++ handlers.py
@@ -43,24 +43,62 @@ async def handle_callback(callback_query):
     elif callback_query.data == "btn2":
         await callback_query.message.reply("Вы нажали Кнопка 2")
 
-# FSM: диалог с пользователем
+
+# FSM: Заполнение формы о пользователе
 @router.message(Command("form"))
 async def start_form(message: Message, state: FSMContext):
-    await message.reply("Как вас зовут?")
-    await state.set_state(Form.name)
+    await message.reply("Введите ваш вес (в кг):")
+    await state.set_state(Form.weight)
 
-@router.message(Form.name)
-async def process_name(message: Message, state: FSMContext):
+@router.message(Form.weight)
+async def process_height(message: Message, state: FSMContext):
     await state.update_data(name=message.text)
-    await message.reply("Сколько вам лет?")
+    await message.reply("Введите ваш рост (в см):")
+    await state.set_state(Form.height)
+
+@router.message(Form.height)
+async def process_age(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.reply("Введите ваш возраст:")
     await state.set_state(Form.age)
 
 @router.message(Form.age)
-async def process_age(message: Message, state: FSMContext):
+async def process_action_minutes(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.reply("Сколько минут активности у вас в день?")
+    await state.set_state(Form.action_minutes)
+
+@router.message(Form.action_minutes)
+async def process_city(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.reply("В каком городе вы находитесь?")
+    await state.set_state(Form.city)
+
+@router.message(Form.city)
+async def process_city(message: Message, state: FSMContext):
+    await state.update_data(name=message.text)
+    await message.reply("Какая у вас цель калорий?")
+    await state.set_state(Form.calories_goal)
+
+# Итог
+@router.message(Form.calories_goal)
+async def process_city(message: Message, state: FSMContext):
     data = await state.get_data()
-    name = data.get("name")
-    age = message.text
-    await message.reply(f"Привет, {name}! Тебе {age} лет.")
+    weight = data.get("weight")
+    height = data.get("height")
+    age = data.get("age")
+    action_minutes = data.get("action_minutes")
+    city =  data.get("city")
+    calories_goal = data.get("calories_goal")
+    # age = message.text
+    await message.reply(f"Итак, \n"
+                        f"- вес: {weight} \n"
+                        f"- рост: {height} \n"
+                        f"- возраст: {age} \n"
+                        f"- кол-во активных минут: {action_minutes} \n"
+                        f"- город: {city} \n"
+                        f"- цель по калориям: {city} \n"
+                        )
     await state.clear()
 
 # Получение шутки из API
--- states.py
+++ states.py
@@ -2,5 +2,9 @@ from aiogram.fsm.state import State, StatesGroup
 
 # форма о пользователе
 class Form(StatesGroup):
-    name = State()
-    age = State()
+    weight = State() # вес
+    height = State() # рост
+    age = State() # возраст
+    action_minutes = State()
+    city = State()
+    calories_goal = State()
